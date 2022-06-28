f8d8163c70069b4f6d9ef7efedcc7726442f06e8
Jaqueline <jaque.arcangelo@gmail.com>
2022-06-28 17:45:29 +0100

    Applying migrations.

---
+++ Library/Library/Author.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Library
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? Nationality { get; set; }
+        public string? Bio { get; set; }
+
+    }
+}
---
+++ Library/Library/LibraryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Library
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Author>? Authors { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=LAPTOPJAQUE\SQLEXPRESS;Database=Library;Trusted_Connection=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
